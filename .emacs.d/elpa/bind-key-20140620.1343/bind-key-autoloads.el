2aa0adbf2ba1c24e39b0c237bc20da77ea5b3fe0
Hugh Van <hvan021@gmail.com>
2014-09-26 17:13:20 +1200

    add emacs packages

---
+++ .emacs.d/elpa/bind-key-20140620.1343/bind-key-autoloads.el
@@ -0,0 +1,18 @@
+;;; bind-key-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("bind-key-pkg.el" "bind-key.el") (21540
+;;;;;;  57753 572000 0))
+
+;;;***
+
+(provide 'bind-key-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; bind-key-autoloads.el ends here
